ad75e352b14649e710f35aed6012d13b7bd204dc
Chris Renfrow <dev@chrisrenfrow.me>
2023-12-01 01:39:27 -0800

    WIP: d01p2 solution
    Not getting the correct answer presently but passes with example input.

--- d01/src/lib.rs
+++ d01/src/lib.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use regex::Regex;
+
 /// Finds the first digit and the last digit on each line to form a two-digit number and sums them all together
 pub fn d01p1(input: String) -> u32 {
     input.lines().fold(0, |acc, line| {
@@ -9,7 +13,63 @@ pub fn d01p1(input: String) -> u32 {
 }
 
 pub fn d01p2(input: String) -> u32 {
-    todo!()
+    let num_map: HashMap<&str, u32> = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    let num_re = Regex::new(r"one|two|three|four|five|six|seven|eight|nine").unwrap();
+
+    input.lines().fold(0, |acc, line| {
+        let mut match_iter = num_re.find_iter(line);
+        let first_match = match_iter.next();
+        // Consume the iterator and return the last match
+        let last_match = num_re.find_iter(line).last();
+        let first_digit = line
+            .chars()
+            .enumerate()
+            .find_map(|(i, c)| match c.to_digit(10) {
+                Some(d) => Some((i, d)),
+                None => None,
+            });
+        let last_digit = line.chars().rev().enumerate().find_map(|(i, c)| {
+            match c.to_digit(10) {
+                // We have to "fix" the index here since enumerate comes after reversing
+                Some(d) => Some((line.len() - i, d)),
+                None => None,
+            }
+        });
+        // Here we figure out which kind of number should be used as first and last based on their order and existence
+        let (first, last) = (
+            match (first_digit, first_match) {
+                // If the first char digit is present and occurs before string digit, use char digit
+                (Some((i, d)), Some(f_mat)) if i < f_mat.start() => d,
+                // If string digit exists use that
+                (_, Some(f_mat)) => *num_map.get(f_mat.as_str()).unwrap(),
+                // Default to char digit
+                (Some((_, d)), None) => d,
+                _ => unreachable!(),
+            },
+            match (last_digit, last_match) {
+                // If the last char digit is present and occurs after string digit, use char digit
+                (Some((i, d)), Some(l_mat)) if i > l_mat.start() => d,
+                // If string digit exists use that
+                (_, Some(f_mat)) => *num_map.get(f_mat.as_str()).unwrap(),
+                // Default to char digit
+                (Some((_, d)), _) => d,
+                _ => unreachable!(),
+            },
+        );
+        // dbg!(&line, &first, &last);
+
+        acc + first * 10 + last
+    })
 }
 
 #[cfg(test)]
@@ -38,6 +98,6 @@ zoneight234
 
     #[test]
     fn part_two() {
-        assert!(d01p2(P2_INPUT.to_string()) == P2_ANSWER)
+        assert_eq!(d01p2(P2_INPUT.to_string()), P2_ANSWER)
     }
 }
