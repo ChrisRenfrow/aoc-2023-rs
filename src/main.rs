eb9d7f67bd5e67014eed698dd578df99eb2c7bbe
Chris Renfrow <dev@chrisrenfrow.me>
2023-12-01 01:39:27 -0800

    WIP: d01p2 solution
    Not getting the correct answer presently but passes with example input.

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,5 @@ fn main() {
     let d01_input = include_str!("../input/d01");
 
     println!("Day 01 Part 1: {}", d01p1(d01_input.to_string()));
+    println!("Day 01 Part 2: {}", d01p2(d01_input.to_string()));
 }
