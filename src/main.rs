988b5f4776a694e745ef63a89a2731b56f1befef
Chris Renfrow <dev@chrisrenfrow.me>
2023-12-06 15:36:09 -0800

    Improve output formatting and DX

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
+use clap::Parser;
+use colored::Colorize;
+
 use d01::*;
 use d02::*;
 use d03::*;
@@ -6,27 +11,77 @@ use d05::*;
 use d06::*;
 
 fn main() {
-    let d01_input = include_str!("../input/d01");
-    println!("Day 01 Part 1: {}", d01p1(d01_input.to_string()));
-    println!("Day 01 Part 2: {}", d01p2(d01_input.to_string()));
+    let cli = Cli::parse();
 
-    let d02_input = include_str!("../input/d02");
-    println!("Day 02 Part 1: {}", d02p1(d02_input.to_string()));
-    println!("Day 02 Part 2: {}", d02p2(d02_input.to_string()));
+    let solutions: Vec<Day> = vec![
+        Day::new(1, Some(&d01p1), Some(&d01p2), day_input(1)),
+        Day::new(2, Some(&d02p1), Some(&d02p2), day_input(2)),
+        Day::new(3, Some(&d03p1), Some(&d03p2), day_input(3)),
+        Day::new(4, Some(&d04p1), None, day_input(4)),
+        Day::new(5, Some(&d05p1), None, day_input(5)),
+        Day::new(6, Some(&d06p1), Some(&d06p2), day_input(6)),
+    ];
 
-    let d03_input = include_str!("../input/d03");
-    println!("Day 03 Part 1: {}", d03p1(d03_input.to_string()));
-    println!("Day 03 Part 2: {}", d03p2(d03_input.to_string()));
+    match cli.day {
+        Some(d) => solutions[d - 1].run(),
+        None => solutions.iter().for_each(|d| d.run()),
+    };
+}
 
-    let d04_input = include_str!("../input/d04");
-    println!("Day 04 Part 1: {}", d04p1(d04_input.to_string()));
-    // println!("Day 04 Part 2: {}", d04p2(d04_input.to_string()));
+type SolutionFn = dyn Fn(String) -> u32;
 
-    let d05_input = include_str!("../input/d05");
-    println!("Day 05 Part 1: {}", d05p1(d05_input.to_string()));
-    // println!("Day 05 Part 2: {}", d05p2(d05_input.to_string()));
+#[derive(Parser)]
+struct Cli {
+    /// A specific day to run
+    #[clap(short, long)]
+    day: Option<usize>,
+}
 
-    let d06_input = include_str!("../input/d06");
-    println!("Day 06 Part 1: {}", d06p1(d06_input.to_string()));
-    println!("Day 06 Part 2: {}", d06p2(d06_input.to_string()));
+#[derive(Clone)]
+struct Day<'a> {
+    num: usize,
+    p1: Option<&'a SolutionFn>,
+    p2: Option<&'a SolutionFn>,
+    input: String,
+}
+
+impl<'a> Day<'a> {
+    fn new(
+        num: usize,
+        p1: Option<&'a SolutionFn>,
+        p2: Option<&'a SolutionFn>,
+        input: String,
+    ) -> Self {
+        Self { num, p1, p2, input }
+    }
+
+    fn run(&self) {
+        println!(
+            "==( {} )===========",
+            format!("Day {:>2}", self.num).yellow()
+        );
+        self.run_part(1);
+        self.run_part(2);
+    }
+
+    fn run_part(&self, part_num: usize) {
+        let part = match part_num {
+            1 => self.p1,
+            2 => self.p2,
+            _ => panic!("There are only 2 parts to any solution"),
+        };
+        match part {
+            Some(part) => println!(
+                "  Part {}: {}",
+                part_num,
+                part(self.input.clone()).to_string().green()
+            ),
+            None => println!("  Part {}: {}", part_num, "SKIPPED".red()),
+        }
+    }
+}
+
+fn day_input(day: usize) -> String {
+    fs::read_to_string(format!("input/d{:02}", day))
+        .expect("Should have been able to read input file")
 }
